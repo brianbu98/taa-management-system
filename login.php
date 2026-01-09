d8da7de7ddffb1d4a54323c8c6c518d527c91a0e
brianbu98 <brianbulahan@gmail.com>
2026-01-09 18:34:38 +0800

    Update login.php

--- login.php
+++ login.php
@@ -4,8 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
-include_once 'connection.php';
+require_once __DIR__ . '/connection.php';
 session_start();
 
 try{
