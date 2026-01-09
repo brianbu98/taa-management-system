133670917ca1f8cdec6cee4ca640c4af3722e028
brianbu98 <brianbulahan@gmail.com>
2026-01-09 19:43:44 +0800

    Fix DB connection variable name

--- connection.php
+++ connection.php
@@ -4,9 +4,9 @@ $username   = "u286236256_u123456_admin";
 $password   = "4~ySBY~g";
 $dbname     = "u286236256_u123456_taaapp";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$con = mysqli_connect($servername, $username, $password, $dbname);
 
-if (!$conn) {
+if (!$con) {
     die("Database connection failed: " . mysqli_connect_error());
 }
-?>
+
