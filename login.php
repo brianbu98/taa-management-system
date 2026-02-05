6801fa30c5a88b3a0d0867fdb5126c9c10ee2989
Brian Bulahan <brianbulahan@gmail.com>
2026-02-06 00:48:51 +0800

    dev done

--- login.php
+++ login.php
@@ -11,7 +11,6 @@ require_once __DIR__ . '/connection.php';
 try{
 
 
-
 $base = dirname($_SERVER['SCRIPT_NAME']);
 
 if (isset($_SESSION['user_id'], $_SESSION['user_type'])) {
@@ -19,9 +18,11 @@ if (isset($_SESSION['user_id'], $_SESSION['user_type'])) {
         case 'admin':
             header("Location: $base/admin/dashboard.php");
             break;
+
         case 'secretary':
             header("Location: $base/secretary/dashboard.php");
             break;
+
         default:
             header("Location: $base/resident/dashboard.php");
     }
