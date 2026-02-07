ed1fcdcefd2b235ca664b3998c9855fc91203b03
Brian Bulahan <brianbulahan@gmail.com>
2026-02-07 13:25:15 +0800

    all nu

--- session.php
+++ session.php
@@ -1,15 +1,15 @@
 ﻿<?php
-/* ======================================================
-   🔐 DEV SESSION HANDLER (FINAL)
-   ====================================================== */
-
 ini_set('session.cookie_httponly', 1);
 ini_set('session.use_strict_mode', 1);
 
 $isHttps = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off');
 ini_set('session.cookie_secure', $isHttps ? 1 : 0);
 
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    session_name('TAA_DEV_SESSION');
-    session_start();
+session_name('TAA_DEV_SESSION');
+
+/* Only start session if allowed */
+if (!isset($NO_SESSION_START)) {
+    if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
+    }
 }
