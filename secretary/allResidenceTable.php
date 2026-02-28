6fbe59596431a4a8f628492202e9d8cbef51ec91
Brian Bulahan <brianbulahan@gmail.com>
2026-03-01 04:28:25 +0800

    done

--- secretary/allResidenceTable.php
+++ secretary/allResidenceTable.php
@@ -9,12 +9,8 @@ $first_name = $con->real_escape_string($_POST['first_name']);
 $middle_name = $con->real_escape_string($_POST['middle_name']);
 $last_name = $con->real_escape_string($_POST['last_name']);
 $status = $con->real_escape_string($_POST['status']);
-$voters =  $con->real_escape_string($_POST['voters']);
 $age =  $con->real_escape_string($_POST['age']);
-$pwd =  $con->real_escape_string($_POST['pwd']);
-$senior =  $con->real_escape_string($_POST['senior']);
 $resident_id =  $con->real_escape_string($_POST['resident_id']);
-$single_parent =  $con->real_escape_string($_POST['single_parent']);
 
 $whereClause = [];
 
@@ -27,21 +23,8 @@ $whereClause[] = "middle_name LIKE '%" .$middle_name. "%'";
 if(!empty($last_name))  
 $whereClause[] = "last_name LIKE '%" .$last_name. "%'";
 
-if(!empty($pwd))  
-$whereClause[] = "residence_status.pwd='".$pwd."'";
-
-if(!empty($single_parent))  
-$whereClause[] = "residence_status.single_parent='".$single_parent."'";
-
-
-if(!empty($senior))  
-$whereClause[] = "residence_status.senior='".$senior."'";
-
 if(!empty($status))  
 $whereClause[] = "residence_status.status='".$status."'";
-
-if(!empty($voters))
-$whereClause[] = "residence_status.voters='".$voters."'";
 
 if(!empty($age))
 $whereClause[] = "residence_information.age='".$age."'";
@@ -69,10 +52,6 @@ residence_information.age,
 residence_information.image, 
 residence_information.image_path, 
 residence_status.residence_id,
-residence_status.voters,
-residence_status.pwd_info,
-residence_status.single_parent,
-residence_status.pwd,
 residence_status.status
 FROM residence_information
 INNER JOIN residence_status ON residence_information.residence_id = residence_status.residence_id
@@ -144,26 +123,11 @@ while($row = $result->fetch_assoc()){
               </label> ';
 }
 
-
-if($row['voters'] == 'YES'){
-  $voters = '<span class="badge badge-success text-md">'.$row['voters'].'</span>';
-}else{
-  $voters = '<span class="badge badge-danger text-md">'.$row['voters'].'</span>';
-}
-
-if($row['single_parent'] == 'YES'){
-  $single_parent = '<span class="badge badge-info text-md ">'.$row['single_parent'].'</span>';
-}else{
-  $single_parent = '<span class="badge badge-warning text-md ">'.$row['single_parent'].'</span>';
-}
   $subdata = [];
   $subdata[] = $image;
   $subdata[] =  $row['residence_id'];
   $subdata[] =  ucfirst($row['first_name']).' '. $middle_name .' '. ucfirst($row['last_name']); 
   $subdata[] =  $row['age'];
-  $subdata[] =  $row['pwd_info']; 
-  $subdata[] =  $single_parent; 
-  $subdata[] = $voters;
   $subdata[] = $status;
   $subdata[] = '<i style="cursor: pointer;  color: yellow;  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;" class="fa fa-user-edit text-lg px-3 viewResidence" id="'.$row['residence_id'].'"></i>
   <i style="cursor: pointer;  color: red;  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;" class="fa fa-times text-lg px-2 deleteResidence" id="'.$row['residence_id'].'"></i>';
