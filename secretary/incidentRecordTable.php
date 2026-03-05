e2dfb0f8047497a3428d48b45f9fb488c4690cc5
Brian Bulahan <brianbulahan@gmail.com>
2026-03-06 02:10:59 +0800

    opti

--- secretary/incidentRecordTable.php
+++ secretary/incidentRecordTable.php
@@ -3,21 +3,32 @@
 include_once '../connection.php';
 
 try {
-    // GET TOTAL RECORDS (NO SEARCH)
-  $sql_total = "SELECT COUNT(*) as total FROM incident_record";
-  $result_total = $con->query($sql_total);
-  $row_total = $result_total->fetch_assoc();
-  $recordsTotal = $row_total['total'];
 
+date_default_timezone_set('Asia/Manila');
 
-  $sql_incident_check = "SELECT * FROM incident_record ";
+/* TOTAL RECORDS */
+$sql_total = "SELECT COUNT(*) as total FROM incident_record";
+$result_total = $con->query($sql_total);
+$row_total = $result_total->fetch_assoc();
+$recordsTotal = $row_total['total'];
 
 $search = $_REQUEST['search']['value'] ?? '';
 
+$sql = "SELECT 
+incidentlog_id,
+status,
+remarks,
+type_of_incident,
+location_incident,
+date_incident,
+date_reported
+FROM incident_record";
+
+/* SEARCH */
 if(!empty($search)){
   $search = $con->real_escape_string($search);
 
-  $sql_incident_check .= " WHERE 
+  $sql .= " WHERE 
     status LIKE '%$search%' OR
     incidentlog_id LIKE '%$search%' OR
     remarks LIKE '%$search%' OR
@@ -28,13 +39,8 @@ if(!empty($search)){
   ";
 }
 
-  $query_incident_check = $con->prepare($sql_incident_check) or die ($con->error);
-  $query_incident_check->execute();
-  $result_incident_check = $query_incident_check->get_result(); 
-  $totalData = $result_incident_check->num_rows;
- $recordsFiltered = $totalData;
-
- $columns = [
+/* ORDER */
+$columns = [
   0 => 'incidentlog_id',
   1 => 'incidentlog_id',
   2 => 'status',
@@ -50,66 +56,60 @@ if(isset($_REQUEST['order'])){
   $columnName = $columns[$columnIndex];
   $columnSortOrder = $_REQUEST['order'][0]['dir'];
 
-  $sql_incident_check .= " ORDER BY $columnName $columnSortOrder ";
-  } else {
-    $sql_incident_check .= ' ORDER BY date_reported DESC ';
-  }
+  $sql .= " ORDER BY $columnName $columnSortOrder";
+}else{
+  $sql .= " ORDER BY date_reported DESC";
+}
 
-  if($_REQUEST['length'] != -1){
-    $sql_incident_check .= ' LIMIT '.
-    $_REQUEST['start'].
-    ' ,' .
-    $_REQUEST['length'] .
-    ' ';
-  }
+/* LIMIT */
+if($_REQUEST['length'] != -1){
+  $sql .= " LIMIT ".$_REQUEST['start']." , ".$_REQUEST['length'];
+}
 
-  $query_incident_check = $con->prepare($sql_incident_check) or die ($con->error);
-  $query_incident_check->execute();
-  $result_incident_check = $query_incident_check->get_result(); 
-  $data = [];
+$query = $con->prepare($sql);
+$query->execute();
+$result = $query->get_result();
 
-  while($row_incident_check = $result_incident_check->fetch_assoc()){
+$data = [];
 
-    date_default_timezone_set('Asia/Manila');
-    $date_incident = date("m/d/Y - h:i A", strtotime($row_incident_check['date_incident']));
-    $date_reported = date("m/d/Y - h:i A", strtotime($row_incident_check['date_reported']));
+while($row = $result->fetch_assoc()){
 
-    if($row_incident_check['status'] == 'NEW'){
-      $status_incident = '<span class="badge badge-primary">'.$row_incident_check['status'].'</span>';
-    } else {
-      $status_incident = '<span class="badge badge-warning">'.$row_incident_check['status'].'</span>';
-    }
+  $date_incident = date("m/d/Y - h:i A", strtotime($row['date_incident']));
+  $date_reported = date("m/d/Y - h:i A", strtotime($row['date_reported']));
 
-    if($row_incident_check['remarks'] == 'CLOSED'){
-      $remarks_incident = '<span class="badge badge-success">'.$row_incident_check['remarks'].'</span>';
-    } else {
-      $remarks_incident = '<span class="badge badge-danger">'.$row_incident_check['remarks'].'</span>';
-    }
+  $status = ($row['status'] == 'NEW')
+  ? '<span class="badge badge-primary">'.$row['status'].'</span>'
+  : '<span class="badge badge-warning">'.$row['status'].'</span>';
 
-    $subdata = [];
-    $subdata[] = '<input type="checkbox" id="'. $row_incident_check['incidentlog_id'].'" class="sub_checkbox">';
-    $subdata[] = $row_incident_check['incidentlog_id'];
-    $subdata[] = $status_incident;
-    $subdata[] = $remarks_incident;
-    $subdata[] = $row_incident_check['type_of_incident'];
-    $subdata[] = $row_incident_check['location_incident'];
-    $subdata[] = $date_incident;
-    $subdata[] = $date_reported;
-    $subdata[] = '<i style="cursor: pointer; color: yellow; text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;" class="fa fa-book-open text-lg px-2 viewRecords" id="'.$row_incident_check['incidentlog_id'].'"></i>';
+  $remarks = ($row['remarks'] == 'CLOSED')
+  ? '<span class="badge badge-success">'.$row['remarks'].'</span>'
+  : '<span class="badge badge-danger">'.$row['remarks'].'</span>';
 
-    $data[] = $subdata;
-  }
+  $subdata = [];
+  $subdata[] = '<input type="checkbox" id="'.$row['incidentlog_id'].'" class="sub_checkbox">';
+  $subdata[] = $row['incidentlog_id'];
+  $subdata[] = $status;
+  $subdata[] = $remarks;
+  $subdata[] = $row['type_of_incident'];
+  $subdata[] = $row['location_incident'];
+  $subdata[] = $date_incident;
+  $subdata[] = $date_reported;
+  $subdata[] = '<i style="cursor:pointer;color:yellow;text-shadow:-1px 0 black,0 1px black,1px 0 black,0 -1px black;" class="fa fa-book-open text-lg px-2 viewRecords" id="'.$row['incidentlog_id'].'"></i>';
 
-  $json_data = [
-  'draw' => intval($_REQUEST['draw']),
-  'recordsTotal' => intval($recordsTotal),
-  'recordsFiltered' => intval($recordsFiltered),
-  'data' => $data,
+  $data[] = $subdata;
+}
+
+$json_data = [
+'draw' => intval($_REQUEST['draw']),
+'recordsTotal' => intval($recordsTotal),
+'recordsFiltered' => intval($recordsTotal),
+'data' => $data
 ];
-  echo json_encode($json_data);
 
-} catch(Exception $e) {
-  echo $e->getMessage();
+echo json_encode($json_data);
+
+} catch(Exception $e){
+ echo $e->getMessage();
 }
 
 ?>
