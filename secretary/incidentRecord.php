bb7c71064f1f962802aab5ec66215f8abba6fe5b
Brian Bulahan <brianbulahan@gmail.com>
2026-03-06 20:31:19 +0800

    preload

--- secretary/incidentRecord.php
+++ secretary/incidentRecord.php
@@ -232,10 +232,11 @@ $logoSrc = (!empty($image_path))
 <body class="hold-transition dark-mode sidebar-mini   layout-footer-fixed">
 <div class="wrapper">
 
-   <!-- Preloader -->
+   <!-- 
    <div class="preloader flex-column justify-content-center align-items-center">
     <img class="animation__wobble " src="../assets/dist/img/loader.gif" alt="AdminLTELogo" height="70" width="70">
   </div>
+  -->
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-dark">
