873c5a769c1f7f2557c71d1970c374ab75676a88
Brian Bulahan <brianbulahan@gmail.com>
2026-03-09 19:25:26 +0800

    d

--- admin/settings.php
+++ admin/settings.php
@@ -5,4 +5,10 @@ error_reporting(E_ALL);
 
 include_once '../connection.php';
 
-echo "CONNECTION LOADED";
+$sql = "SELECT * FROM taa_information LIMIT 1";
+
+$result = $con->query($sql);
+
+$row = $result->fetch_assoc();
+
+print_r($row);
