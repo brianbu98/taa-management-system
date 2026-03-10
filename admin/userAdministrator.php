ea5bc1a3d66343ba8cc46b65e82eb09aa7743a9b
Brian Bulahan <brianbulahan@gmail.com>
2026-03-10 19:44:57 +0800

    wwe

--- admin/userAdministrator.php
+++ admin/userAdministrator.php
@@ -1,5 +1,4 @@
- c
-<?php 
+ <?php 
 
 include_once '../connection.php';
 session_start();
@@ -32,7 +31,10 @@ try{
       $image_path = $row['image_path'];
       $id = $row['id'];
   }
-  
+  $logoSrc = (!empty($image_path))
+    ? '../' . ltrim($image_path, '/')
+    : '../assets/logo/logo.png';
+
   
   }else{
    echo '<script>
@@ -253,10 +255,10 @@ try{
             <!-- Message Start -->
             <div class="media">
               <?php 
-                if($user_image != '' || $user_image != null || !empty($user_image)){
-                  echo '<img src="../assets/dist/img/'.$user_image.'" class="img-size-50 mr-3 img-circle alt="User Image">';
+                if(!empty($image)){
+                  echo '<img src="../assets/dist/img/'.$user_image.'" class="img-size-50 mr-3 img-circle" alt="User Image">';
                 }else{
-                  echo '<img src="../assets/dist/img/image.png" class="img-size-50 mr-3 img-circle alt="User Image">';
+                  echo '<img src="../assets/dist/img/image.png" class="img-size-50 mr-3 img-circle" alt="User Image">';
                 }
               ?>
             
@@ -279,15 +281,13 @@ try{
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4 sidebar-no-expand">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link text-center">
-    <?php 
-        if($image != '' || $image != null || !empty($image)){
-          echo '<img src="'.$image_path.'" id="logo_image" class="img-circle elevation-5 img-bordered-sm" alt="logo" style="width: 70%;">';
-        }else{
-          echo ' <img src="../assets//logo//logo.png" id="logo_image" class="img-circle elevation-5 img-bordered-sm" alt="logo" style="width: 70%;">';
-        }
-
-      ?>
+  <a href="#" class="brand-link text-center">
+    <img src="<?= htmlspecialchars($logoSrc) ?>"
+         id="logo_image"
+         class="img-circle elevation-5 img-bordered-sm"
+         alt="logo"
+         style="width:70%;">
+</a>
       <span class="brand-text font-weight-light"></span>
     </a>
 
@@ -297,7 +297,9 @@ try{
 
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../assets/dist/img/logo.png" class="img-circle elevation-5 img-bordered-sm" alt="User Image">
+          <img src="<?= htmlspecialchars($logoSrc) ?>"
+     class="img-circle elevation-5 img-bordered-sm"
+     alt="Admin Logo">
         </div>
         <div class="info text-center">
           <a href="#" class="d-block text-bold"><?= strtoupper($user_type) ?></a>
