370c1e5a227568a61bc7e950a21edc79cfe9c632
Brian Bulahan <brianbulahan@gmail.com>
2026-03-10 19:13:11 +0800

    wwe

--- admin/position.php
+++ admin/position.php
@@ -273,13 +273,15 @@ $logoSrc = (!empty($image_path))
     
 
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="../assets/dist/img/logo.png" class="img-circle elevation-5 img-bordered-sm" alt="User Image">
-        </div>
-        <div class="info text-center">
-          <a href="#" class="d-block text-bold"><?= strtoupper($user_type) ?></a>
-        </div>
-      </div>
+    <div class="image">
+        <img src="<?= htmlspecialchars($logoSrc) ?>"
+             class="img-circle elevation-5 img-bordered-sm"
+             alt="Admin Logo">
+    </div>
+    <div class="info text-center">
+        <a href="#" class="d-block text-bold"><?= strtoupper($user_type) ?></a>
+    </div>
+</div>
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
