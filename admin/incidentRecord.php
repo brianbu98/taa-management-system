bc173dae142580734db136a6ffd0f6ec4b99f25c
Brian Bulahan <brianbulahan@gmail.com>
2026-03-10 18:44:32 +0800

    new lgo

--- admin/incidentRecord.php
+++ admin/incidentRecord.php
@@ -42,6 +42,10 @@ try{
         $image_path = $row['image_path'];
         $id = $row['id'];
     }
+    $logoSrc = (!empty($image_path))
+    ? '../' . ltrim($image_path, '/')
+    : '../assets/logo/logo.png';
+
 
 }catch(Exception $e){
     die($e->getMessage());
@@ -280,14 +284,12 @@ try{
   <aside class="main-sidebar sidebar-dark-primary elevation-4 sidebar-no-expand">
     <!-- Brand Logo -->
     <a href="#" class="brand-link text-center">
-    <?php 
-        if(!empty($image)){
-          echo '<img src="'.$image_path.'" id="logo_image" class="img-circle elevation-5 img-bordered-sm" alt="logo" style="width: 70%;">';
-        }else{
-          echo ' <img src="../assets//logo//logo.png" id="logo_image" class="img-circle elevation-5 img-bordered-sm" alt="logo" style="width: 70%;">';
-        }
-
-      ?>
+    <img src="<?= htmlspecialchars($logoSrc) ?>"
+         id="logo_image"
+         class="img-circle elevation-5 img-bordered-sm"
+         style="width:70%;"
+         alt="Logo">
+</a>
       <span class="brand-text font-weight-light"></span>
     </a>
 
@@ -297,7 +299,9 @@ try{
 
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../assets/dist/img/logo.png" class="img-circle elevation-5 img-bordered-sm" alt="User Image">
+         <img src="<?= htmlspecialchars($logoSrc) ?>"
+     class="img-circle elevation-5 img-bordered-sm"
+     alt="Admin Logo"> 
         </div>
         <div class="info text-center">
           <a href="#" class="d-block text-bold"><?= strtoupper($user_type) ?></a>
