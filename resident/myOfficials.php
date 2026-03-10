a800562141fc403903193c5cf385ef5a90c2a3cc
Brian Bulahan <brianbulahan@gmail.com>
2026-03-10 20:32:46 +0800

    wwv

--- resident/myOfficials.php
+++ resident/myOfficials.php
@@ -1,16 +1,10 @@
 <?php
-require_once '../session.php';
 require_once '../connection.php';
+session_start();
 
 if(isset($_SESSION['user_id']) && $_SESSION['user_type'] == 'resident'){
 
-$sql = "SELECT 
-official.*, position.position 
-FROM official 
-LEFT JOIN position ON official.position_id = position.position_id
-WHERE official.status = 'Active'
-ORDER BY position.position_id ASC";
-
+$sql = "SELECT * FROM official WHERE status = 'Active'";
 $result = $con->query($sql);
 
 }else{
@@ -21,9 +15,11 @@ echo "<script>window.location.href='../login.php'</script>";
 <!DOCTYPE html>
 <html>
 <head>
-<title>Homeowner Officials</title>
+<title>Homeowners Officials</title>
+
 <link rel="stylesheet" href="../assets/dist/css/adminlte.min.css">
 <link rel="stylesheet" href="../assets/plugins/fontawesome-free/css/all.min.css">
+
 </head>
 
 <body class="hold-transition layout-top-nav">
@@ -40,12 +36,12 @@ CURRENT HOMEOWNERS OFFICIALS
 
 <div class="col-md-3 text-center mb-4">
 
-<img src="../assets/dist/img/<?php echo $row['image']; ?>"
+<img src="../assets/dist/img/<?php echo $row['image']; ?>" 
 class="img-circle"
 width="120">
 
 <h5 class="mt-2">
-<?php echo $row['first_name'].' '.$row['last_name']; ?>
+<?php echo $row['first_name']." ".$row['last_name']; ?>
 </h5>
 
 <p class="text-muted">
