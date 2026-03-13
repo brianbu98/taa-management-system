a02a9d84a6937d8c8867c49045c26f54bd19f90d
Brian Bulahan <brianbulahan@gmail.com>
2026-03-13 11:23:57 +0800

    best

--- admin/addNewOfficial.php
+++ admin/addNewOfficial.php
@@ -4,8 +4,10 @@ include_once '../connection.php';
 session_start();
 
 try{
-  if(isset($_SESSION['user_id']) && isset($_SESSION['user_type']) && $_SESSION['user_type'] == 'admin'){
 
+    if(!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'admin'){
+      exit('unauthorized');
+  }
     $user_id = $_SESSION['user_id'];
     $sql_user = "SELECT * FROM `users` WHERE `id` = ? ";
     $stmt_user = $con->prepare($sql_user) or die ($con->error);
@@ -15,55 +17,57 @@ try{
     $row_user = $result_user->fetch_assoc();
     $first_name_user = $row_user['first_name'];
     $last_name_user = $row_user['last_name'];
-  }else{
-    echo '<script>
-           window.location.href = "../login.php";
-         </script>';
-   }
- 
-  }
+
   
-$add_position = $con->real_escape_string($_POST['add_position']);
-$add_first_name = $con->real_escape_string($_POST['add_first_name']);
-$add_middle_name = $con->real_escape_string($_POST['add_middle_name']);
-$add_last_name = $con->real_escape_string($_POST['add_last_name']);
-$add_suffix = $con->real_escape_string($_POST['add_suffix']);
-$add_gender = $con->real_escape_string($_POST['add_gender']);
-$add_civil_status = $con->real_escape_string($_POST['add_civil_status']);
-$add_religion = $con->real_escape_string($_POST['add_religion']);
-$add_nationality = $con->real_escape_string($_POST['add_nationality']);
-$add_contact_number = $con->real_escape_string($_POST['add_contact_number']);
-$add_email_address = $con->real_escape_string($_POST['add_email_address']);
-$add_address = $con->real_escape_string($_POST['add_address']);
-$add_birth_date = $con->real_escape_string($_POST['add_birth_date']);
-$add_birth_place = $con->real_escape_string($_POST['add_birth_place']);
-$add_province = $con->real_escape_string($_POST['add_province']);
-$add_zip = $con->real_escape_string($_POST['add_zip']);
-$add_city = $con->real_escape_string($_POST['add_city']);
-$add_house_number = $con->real_escape_string($_POST['add_house_number']);
-$add_street = $con->real_escape_string($_POST['add_street']);
-$add_fathers_name = $con->real_escape_string($_POST['add_fathers_name']);
-$add_mothers_name = $con->real_escape_string($_POST['add_mothers_name']);
-$add_guardian = $con->real_escape_string($_POST['add_guardian']);
-$add_guardian_contact = $con->real_escape_string($_POST['add_guardian_contact']);
-$add_image = $con->real_escape_string($_FILES['add_image']['name']);
-$add_status = 'ACTIVE';
-$add_approval = 'ACCEPTED';
+    $add_position = $_POST['add_position'] ?? '';
+    $add_first_name = $_POST['add_first_name'] ?? '';
+    $add_middle_name = $_POST['add_middle_name'] ?? '';
+    $add_last_name = $_POST['add_last_name'] ?? '';
+    $add_suffix = $_POST['add_suffix'] ?? '';
+    $add_gender = $_POST['add_gender'] ?? '';
+    $add_civil_status = $_POST['add_civil_status'] ?? '';
+    $add_religion = $_POST['add_religion'] ?? '';
+    $add_nationality = $_POST['add_nationality'] ?? '';
+    $add_contact_number = $_POST['add_contact_number'] ?? '';
+    $add_email_address = $_POST['add_email_address'] ?? '';
+    $add_address = $_POST['add_address'] ?? '';
+    $add_birth_date = $_POST['add_birth_date'] ?? '';
+    $add_birth_place = $_POST['add_birth_place'] ?? '';
+    $add_province = $_POST['add_province'] ?? '';
+    $add_zip = $_POST['add_zip'] ?? '';
+    $add_city = $_POST['add_city'] ?? '';
+    $add_house_number = $_POST['add_house_number'] ?? '';
+    $add_street = $_POST['add_street'] ?? '';
+    $add_fathers_name = $_POST['add_fathers_name'] ?? '';
+    $add_mothers_name = $_POST['add_mothers_name'] ?? '';
+    $add_guardian = $_POST['add_guardian'] ?? '';
+    $add_guardian_contact = $_POST['add_guardian_contact'] ?? '';
+    $add_image = isset($_FILES['add_image']['name']) ? $_FILES['add_image']['name'] : '';
+    $add_status = 'ACTIVE';
+    $add_approval = 'ACCEPTED';
 
 
-
-if(isset($add_image)){
-  if($add_image != '' || $add_image != null || !empty($add_image)){
-    $type = explode('.', $add_image);
-    $type = $type[count($type) -1];
-    $new_image_name = uniqid(rand()) .'.'. $type;
-    $new_image_path = '../assets/dist/img/' . $new_image_name;
-    move_uploaded_file($_FILES['add_image']['tmp_name'],$new_image_path);
-  }else{
-    $new_image_name = '';
-    $new_image_path = '';
-  }
+    /* VALIDATION */
+if(empty($add_position)){
+    exit('error');
 }
+
+
+if(!empty($add_image) && isset($_FILES['add_image']['tmp_name'])){
+
+  $type = pathinfo($add_image, PATHINFO_EXTENSION);
+  $new_image_name = uniqid(rand(), true) . '.' . $type;
+  $new_image_path = '../assets/dist/img/' . $new_image_name;
+
+  move_uploaded_file($_FILES['add_image']['tmp_name'],$new_image_path);
+
+}else{
+
+  $new_image_name = '';
+  $new_image_path = '';
+
+}
+
 
 $sql_position = "SELECT COUNT(position) AS count_position  FROM official_status WHERE position = ?";
 $stmt_position = $con->prepare($sql_position) or die ($con->error);
@@ -79,10 +83,9 @@ $stmt_position_limit->execute();
 $result_position_limit = $stmt_position_limit->get_result();
 $row_position_limit = $result_position_limit->fetch_assoc();
 
-if($row_position_limit['position_limit'] == $row_position['count_position']){
+if($row_position['count_position'] >= $row_position_limit['position_limit']){
   exit('error');
 }
-
 
 
 
@@ -92,9 +95,14 @@ if($row_position_limit['position_limit'] == $row_position['count_position']){
 date_default_timezone_set('Asia/Manila');
 $date = new DateTime();
 
-$today = date("Y/m/d");
-$age = date_diff(date_create($add_birth_date), date_create($today));
-$add_age_date = $age->format("%y");
+$today = date("Y-m-d");
+
+if(!empty($add_birth_date)){
+    $age = date_diff(date_create($add_birth_date), date_create($today));
+    $add_age_date = $age->format("%y");
+}else{
+    $add_age_date = 0;
+}
 
 
 $official_id = $date->format("mdYHisv").$add_age_date;
