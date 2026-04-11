1ef8a87c19bf0979229cb45d07aec9b2ffdf41e1
Brian Bulahan <brianbulahan@gmail.com>
2026-04-11 17:34:06 +0800

    save

--- admin/incidentRecord.php
+++ admin/incidentRecord.php
@@ -742,6 +742,15 @@ try{
 <script src="../assets/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap -->
 <script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- 3. DataTables -->
+<script src="/dev/assets/plugins/datatables/jquery.dataTables.min.js"></script>
+<!-- 4. DataTables Bootstrap -->
+<script src="/dev/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script>
+console.log("DataTable loaded:", typeof $.fn.dataTable);
+</script>
+<!-- 5. SweetAlert -->
+<script src="/dev/assets/plugins/sweetalert2/sweetalert2.all.min.js"></script>
 <!-- overlayScrollbars -->
 <script src="../assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
